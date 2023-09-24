d71c641f5a4697c961c56e28b394bdd0799561b1
Hrithik <slumdog4321@gmail.com>
2023-09-24 23:36:53 +0530

    basic Clerk Auth implemented with sign in and sign up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs/app-beta";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold ">Welcome To Mind It A Reminder App</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
