1731d1b8264aa0edb561b2e5dd298a0c6e208b4c
Hrithik <slumdog4321@gmail.com>
2023-09-25 00:05:17 +0530

    added Navbar, themeSwitcher, Shadcn for Styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Separator } from "@/components/ui/separator";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import NavBar from "@/components/NavBar";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +17,30 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html
+        lang="en"
+        className={cn(inter.className, "dark")}
+        style={{
+          colorScheme: "dark",
+        }}
+      >
+        <body>
+          <ThemeProvider>
+            <div
+              className="
+             flex
+             min-h-screen
+             w-full
+             flex-col
+             items-center
+             dark:bg-black"
+            >
+              <NavBar />
+              <Separator />
+              {children}
+            </div>
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
